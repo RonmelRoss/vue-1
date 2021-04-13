6dbea74ca614f77ac99813b774cf2e8b530c2c67
Ronmel Ross <ronmelross@gmail.com>
2021-04-13 13:45:16 +0800

    #12 Reusing Components With Props

---
+++ plan-picker/app.js
@@ -0,0 +1,17 @@
+Vue.component('plan', {
+    template: '#plan-template',
+    props: {
+      name: {
+        type: String,
+        required: true
+      }
+    }
+  })
+  
+  new Vue({
+    el: '#app',
+    data: {
+      plans: ['The Single', 'The Curious', 'The Addict']
+    }
+  }) 
+  
